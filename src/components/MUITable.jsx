c35c95542a52fc356fc9021dc0822ec68af9266d
Birgül Özhan <birgulozhan9@gmail.com>
2025-09-15 18:56:19 +0300

    tablo genişletildi ve table tasarımı düzenlendi.

--- src/components/MUITable.jsx
+++ src/components/MUITable.jsx
@@ -1,73 +1,83 @@
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import axios from 'axios';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
 import CircularProgress from "@mui/material/CircularProgress"; //loading spinner için kullanılan component
+import Box from "@mui/material/Box";
 
 const MUITable = () => {
+  const [rows, setRows] = useState([]);
+  const [loading, setLoading] = useState(true); // loading durumunu tutalım
 
-    /*const rows = [
-        {id : 1, firstName: "Birgül", lastName : "Demirden", age : 26},
-        {id : 2, firstName: "Birgül", lastName : "Demirden", age : 26},
-        {id :3, firstName: "Birgül", lastName : "Demirden", age : 26},
-    ]*/
-
-    const [rows, setRows] = useState([]);
-    const [loading, setLoading] = useState(true); // loading durumunu tutalım
-
-    useEffect(() => {
-      setTimeout(() => {
-        axios.get("https://api.ikkutusu.com.tr/api/QrCode")
+  useEffect(() => {
+    setTimeout(() => {
+      axios
+        .get("https://api.ikkutusu.com.tr/api/QrCode")
         .then((response) => {
-          //console.log("Api test", response.data)
-          //setRows(response.data)
-          setRows(response.data.result)
+          setRows(response.data.result);
           setLoading(false);
         })
         .catch((error) => {
-          alert("veri çekme hatasi",error.message)
-          setLoading(false)
-        })
-      }, 2000)
-    },[]);
+          alert("veri çekme hatasi", error.message);
+          setLoading(false);
+        });
+    }, 2000);
+  }, []);
 
-    if(loading) {
-      return(
-        <div>
-          <CircularProgress />
-        </div>
-      )
-    }
+  if (loading) {
+    return (
+      <div>
+        <CircularProgress />
+      </div>
+    );
+  }
 
   return (
-    <div>
-      <TableContainer>
-        <Table>
+    <>
+      <h2 style={{ marginBottom: "5px", padding: "20px" }}>QR Kod Listesi</h2>
+      <Box
+        component="section"
+        sx={{ p:3 }}
+      >
+        <TableContainer component={Paper}>
+          <Table stickyHeader>
             <TableHead>
-                <TableRow>
-                    <TableCell>ID</TableCell>
-                    <TableCell>İÇERİK</TableCell>
-                </TableRow>
+              <TableRow style={{ background: "lightgrey" }}>
+                <TableCell>ID</TableCell>
+                <TableCell>LOKASYON ADI</TableCell>
+                <TableCell>ENLEM</TableCell>
+                <TableCell>BOYLAM</TableCell>
+                <TableCell>KULLANIM ALANI</TableCell>
+                <TableCell>AÇIKLAMA</TableCell>
+              </TableRow>
             </TableHead>
             <TableBody>
-                {rows.map((row) => (
-                    <TableRow key={row.id}>
-                        <TableCell>{row.id}</TableCell>
-                        <TableCell>{row.description}</TableCell>
-                    </TableRow>
-                ))}
+              {rows.map((row) => (
+                <TableRow 
+                  key={row.id}
+                  sx={{ "&:nth-of-type(odd)": { backgroundColor: "action.hover" } }}
+                  >
+                  <TableCell>{row.id}</TableCell>
+                  <TableCell>{row.locationName}</TableCell>
+                  <TableCell>{row.latitude}</TableCell>
+                  <TableCell>{row.longitude}</TableCell>
+                  <TableCell>{row.useArea}</TableCell>
+                  <TableCell>{row.description}</TableCell>
+                </TableRow>
+              ))}
             </TableBody>
-        </Table>
-      </TableContainer>
-    </div>
-  )
-}
+          </Table>
+        </TableContainer>
+      </Box>
+    </>
+  );
+};
 
-export default MUITable
+export default MUITable;
