c3477c69bcd3a08b933e3296db240785d4acb38d
Birgül Özhan <birgulozhan9@gmail.com>
2025-09-16 19:33:13 +0300

    form oluşturuldu

--- src/components/QrCodeDialog.jsx
+++ src/components/QrCodeDialog.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import z from 'zod';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Dialog,
@@ -10,65 +10,104 @@ import {
   DialogActions,
   Button,
   Box,
-  TextField
+  TextField,
 } from "@mui/material";
 
 //Schema tanımı
 const schema = z.object({
-  locatinName: z.string().min(1, "Lokasyon adı Zorunludur."),
-  useArea: z.string.min(1, "Kullanım alanı zorunlduur."),
-  latitude: z.string(1, "Enlem zorunludur."),
+  locationName: z.string().min(1, "Lokasyon adı Zorunludur."),
+  useArea: z.string().min(1, "Kullanım alanı zorunlduur."),
+  latitude: z.string().min(1, "Enlem zorunludur."),
   longitude: z.string().min(1, "Boylam zorunludur."),
-  description: z.string().optional()
+  description: z.string().optional(),
 });
 
-const QrCodeDialog = ({open, handleClose}) => {
+const QrCodeDialog = ({ open, handleClose }) => {
+  // useForm ile formu yönetelim
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
+
+  // Submit fonksiyonu
+  const onSubmit = (data) => {
+    console.log("Form verisi:", data);
+    // burada api.post("/QrCode/generate", data) yapabiliriz.
+    handleClose(); // form submitten sonra diaalog kapanır
+  };
+
   return (
     <div>
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>Yeni QR Kod Ekle</DialogTitle>
         <DialogContent>
-          <DialogContentText sx={{ display: "flex", flexDirection: "column", gap: 2, mt: 1}}>
-            {/* Lokasyon Adı */}
-            <TextField 
-              label="Lokasyon Adı"
-              fullWidth
-              variant='outlined'
-            />
-
-            {/* Kullanım Alanı */}
-            <TextField 
-              label="Kullanım Alanı"
-              fullWidth
-              variant='outlined'
-            />
-
-            {/* Lokasyon */}
+          <form onSubmit={handleSubmit(onSubmit)}>
             <Box>
-              <TextField 
+              {/* Lokasyon Adı */}
+              <TextField
+                label="Lokasyon Adı"
+                fullWidth
+                variant="outlined"
+                {...register("locationName")}
+                error={!!errors.locationName}
+                sx={{ mb: 2, mt: 2 }}
+                helperText={errors.locationName?.message}
+              />
+
+              {/* Kullanım Alanı */}
+              <TextField
+                label="Kullanım Alanı"
+                fullWidth
+                variant="outlined"
+                {...register("useArea")}
+                sx={{ mb: 2 }}
+                helperText={errors.useArea?.message}
+              />
+
+              {/* Lokasyon */}
+              <TextField
                 label="Enlem"
                 fullWidth
-                variant='outlined'
-                sx={{mb:2}}
+                variant="outlined"
+                {...register("latitude")}
+                error={!!errors.latitude}
+                helperText={errors.latitude?.message}
+                sx={{ mb: 2 }}
               />
-              <TextField 
+
+              <TextField
                 label="Boylam"
                 fullWidth
-                variant='outlined'
-                sx={{mb:2}}
+                variant="outlined"
+                {...register("longitude")}
+                error={!!errors.longitude}
+                helperText={errors.longitude?.message}
+                sx={{ mb: 2 }}
+              />
+
+              <TextField
+                label="Açıklama"
+                fullWidth
+                variant="outlined"
+                {...register("description")}
+                sx={{ mb: 2 }}
               />
             </Box>
-          </DialogContentText>
 
-          {/* Form inputları buraya gelecek. */}
+            <DialogActions>
+              <Button onClick={handleClose}>Kapat</Button>
+              <Button type="submit" variant="contained">
+                Oluştur
+              </Button>
+            </DialogActions>
+          </form>
         </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Kapat</Button>
-          <Button onClick={handleClose}>Oluştur</Button>
-        </DialogActions>
       </Dialog>
     </div>
-  )
-}
+  );
+};
 
-export default QrCodeDialog
+export default QrCodeDialog;
