03b320bb5cce7fe1daada13289696f7d279e0981
Birgül Özhan <birgulozhan9@gmail.com>
2025-09-22 11:11:53 +0300

    App.js içine QueryClientProvider eklendi.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import MUITable from './components/MUITable'
+import MUITable from "./components/MUITable";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// QueryClient -> React Query'nin merkezi yöneticisi
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <MUITable />
+      <QueryClientProvider client={queryClient}>
+        <MUITable />
+      </QueryClientProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
