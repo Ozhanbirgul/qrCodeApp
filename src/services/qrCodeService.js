39e5d7cb549a915f4c2dbd60e3e38359ba9dde85
Birgül Özhan <birgulozhan9@gmail.com>
2025-09-22 21:01:12 +0300

    servisler try-catch bloğuna alındı

--- src/services/qrCodeService.js
+++ src/services/qrCodeService.js
@@ -1,11 +1,23 @@
 import api from "../api/axios";
 
 // Tüm Qr kodlarını getirir
-export const getQrCodes = () => {
-    return api.get("/QrCode"); // axios ile /QrCode endpointine GET isteği gönderiliyor.
-};
+export const getQrCodes = async () => {
+    try {
+        const res = await api.get("/QrCode");
+        return res.data; // response.data'yı döndürür
+    } catch (error) {
+        console.error("Qr kodları çekme hatası:", error);
+        throw error; // useQuery içinde yakalanacak
+    }
+}
 
 // Yeni Qr kodalrını oluşturur
-export const createQrCode = (data) => {
-    return api.post("/QrCode/generate", data);
-} 
+export const createQrCode = async (data) => {
+    try {
+        const res = await api.post("/QrCode/generate", data);
+        return res.data;
+    } catch (error) {
+        console.error("Qr kod oluşturma hatası:", error);
+        throw error; // useMutation içinde yakalnacak
+    }
+}
